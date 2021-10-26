2e972673cb967e96beb4924206580a35bac2b307
liadensam <li.adensam@gmail.com>
2021-10-26 00:29:20 +0000

    to finish readme file

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const form = document.querySelector("form");
 const getName = document.querySelector("#entername");
 const submitBtn = document.querySelector("#submitname");
 const forgetBtn = document.querySelector("#forgetname");
+const hideBtn = document.querySelector("#hideBtn");
+const showBtn = document.querySelector("#showBtn")
 
 const h1 = document.querySelector("h1");
 const greeting = document.querySelector(".personal-greeting");
@@ -22,6 +24,25 @@ forgetBtn.addEventListener('click', function() {
   localStorage.removeItem('name', getName.value);
 
   nameDisplayCheck();
+});
+
+hideBtn.addEventListener('click', function(){
+  let img = document.querySelector(".gif");
+  let hideText = document.querySelector(".hide p");
+  img.style.display = "none";
+  showBtn.style.opacity = "1";
+  hideBtn.style.opacity = "0";
+  hideText.style.display = "none"
+});
+
+showBtn.addEventListener('click', function(){
+  let img = document.querySelector(".gif");
+  let hideText = document.querySelector(".hide p");
+  img.style.display = "";
+  showBtn.style.opacity = "0";
+  hideBtn.style.opacity = "1"
+  hideText.style.display = ""
+
 });
 
 function nameDisplayCheck() {
